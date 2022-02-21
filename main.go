2cd9921fba6f18c4ec4347623d5c21443778ec8d
pi-rate14 <apoorva.srivastava2019@vitstudent.ac.in>
2022-02-21 13:40:54 +0530

    initial commit

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	routes.UserRoutes(router)
+	router.Use(middleware.Authenticaction())
+
+	routes.FoodRoutes(router)
+	routes.MeuRoutes(router)
+	routes.TableRoutes(router)
+	routes.OrderRoutes(router)
+	routes.OrderItemRoutes(router)
+	routes.InoviceRoutes(router)
+
+	router.Run(":"+port)
+}
---
+++ routes/userRouter.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	controller "github.com/pi-rate14/stove-backend/controllers"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine){
+	incomingRoutes.GET("/users", controller.GetUsers())
+	incomingRoutes.GET("/users/:user_id", controller.GetUser())
+
+	incomingRoutes.POST("/users/signup", controller.Signup())
+	incomingRoutes.POST("/users/login", controller.Login())
+}
