329a0d29e1c96fbc5b5eca84e5e1cadca2591e78
pi-rate14 <apoorva.srivastava2019@vitstudent.ac.in>
2022-02-23 10:46:57 +0530

    order controller

--- controllers/tableController.go
+++ controllers/tableController.go
@@ -1,6 +1,13 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/pi-rate14/stove-backend/database"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var tableCollection *mongo.Collection = database.OpenCollection(database.Client, "table")
+
 
 func GetTables() gin.HandlerFunc{
 	return func(c *gin.Context){
