d33151be5c5c4a97e45057f5e8cf6c42bf3f9530
pi-rate14 <apoorva.srivastava2019@vitstudent.ac.in>
2022-02-24 10:13:06 +0530

    final commit

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func main() {
 	router := gin.New()
 	router.Use(gin.Logger())
 	routes.UserRoutes(router)
-	router.Use(middleware.Authenticaction())
+	router.Use(middleware.Authentication())
 
 	routes.FoodRoutes(router)
 	routes.MenuRoutes(router)
